1da8df613e71024eb847c6f86ffdbb24403533f2
Henry Steinhauer <henry.steinhauer@i22.de>
2020-04-25 17:17:07 +0200

    feat: Create footer component

---
+++ components/footer.vue
@@ -0,0 +1,107 @@
+<template>
+  <v-container>
+    <hr>
+    <v-row justify="center">
+      <v-col
+        cols="12"
+        md="6"
+        lg="6"
+        xl="5"
+      >
+        <v-row class="mt-4">
+          <v-col
+            cols="12"
+            md="6"
+            xl="5"
+          >
+            <p class="body-1 font-weight-medium">
+              Persönlichkeitscoaching &
+            </p>
+            <p class="body-1 font-weight-medium">
+              Unternehmenscoaching Bonn
+            </p>
+          </v-col>
+          <v-col
+            cols="12"
+            sm="6"
+            xl="5"
+          >
+            <p class="body-1 font-weight-medium">
+              Claudia Eck
+            </p>
+            <p class="body-1 font-weight-medium">
+              Systemischer Coach
+            </p>
+            <p class="body-1 font-weight-medium">
+              Dipl. Oecotrophologin
+            </p>
+          </v-col>
+        </v-row>
+        <v-row align="end">
+          <v-col
+            cols="12"
+            md="6"
+            xl="5"
+          >
+            <p class="body-1 font-weight-medium">
+              Claudia Eck
+            </p>
+            <p class="body-1 font-weight-medium">
+              Systemischer Coach
+            </p>
+            <p class="body-1 font-weight-medium">
+              Dipl. Oecotrophologin
+            </p>
+          </v-col>
+          <v-col
+            cols="12"
+            md="6"
+            xl="5"
+          >
+            <a
+              href="tel:01792 242543"
+              target="_blank"
+              class="body-1 font-weight-medium"
+            >+ 1792 242543</a>
+            <br>
+            <a
+              href="mailto:info@claudiaeck.de"
+              target="_blank"
+              class="body-1 font-weight-medium"
+            >info@claudiaeck.de</a>
+          </v-col>
+        </v-row>
+      </v-col>
+      <v-col
+        cols="12"
+        md="6"
+        lg="6"
+        xl="5"
+      >
+        <v-row
+          class="mt-4"
+          align="end"
+        >
+          <v-col>
+            <p
+              class="body-1 font-weight-medium text-right"
+            >
+              Persönlichkeitscoaching &
+            </p>
+            <p
+              class="body-1 font-weight-medium text-right"
+            >
+              Unternehmenscoaching Bonn
+            </p>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
