4ff9146400548b6d360a0d0f92b16048f968ff2b
Henry Steinhauer <henry.steinhauer@i22.de>
2020-07-04 22:48:02 +0200

    feat: Add mailer config

--- middleware/helper/MailerActions.js
+++ middleware/helper/MailerActions.js
@@ -1,11 +1,14 @@
-import nodemailer from 'nodemailer'
+const nodemailer = require('nodemailer')
+require('dotenv').config()
 
 export class MailerActions {
   sendMail (name, email, msg) {
     const transporter = nodemailer.createTransport({
-      sendmail: true,
-      newline: 'unix',
-      path: '/usr/sbin/sendmail'
+      service: 'gmail',
+      auth: {
+        user: process.env.USER,
+        pass: process.env.PW
+      }
     })
     transporter.sendMail({
       from: email,
