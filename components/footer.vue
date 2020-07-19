4262c08dd16af64a8aa9b34c292e36cee9bac4c3
Henry Steinhauer <henry.steinhauer@i22.de>
2020-07-19 13:37:11 +0200

    fix: Edit styling and content from footer

--- components/footer.vue
+++ components/footer.vue
@@ -1,106 +1,141 @@
 <template>
-  <v-container class="pa-0">
+  <div>
     <hr>
-    <v-row
-      justify="center"
-      class="footer"
-    >
-      <v-col
-        cols="11"
-        md="6"
-        lg="6"
-        xl="5"
+    <v-container class="pa-0">
+      <v-row
+        justify="center"
+        class="footer"
       >
-        <v-row class="mt-4">
-          <v-col
-            cols="12"
-            md="6"
-            xl="5"
-          >
-            <p class="body-1 font-weight-medium">
-              Persönlichkeitscoaching &
-            </p>
-            <p class="body-1 font-weight-medium">
-              Unternehmenscoaching Bonn
-            </p>
-          </v-col>
-          <v-col
-            cols="12"
-            sm="6"
-            xl="5"
-          >
-            <p class="body-1 font-weight-medium">
-              Claudia Eck
-            </p>
-            <p class="body-1 font-weight-medium">
-              Systemischer Coach
-            </p>
-            <p class="body-1 font-weight-medium">
-              Dipl. Oecotrophologin
-            </p>
-          </v-col>
-        </v-row>
-        <v-row align="end">
-          <v-col
-            cols="12"
-            md="6"
-            xl="5"
-          >
-            <p class="body-1 font-weight-medium">
-              Claudia Eck
-            </p>
-            <p class="body-1 font-weight-medium">
-              Systemischer Coach
-            </p>
-            <p class="body-1 font-weight-medium">
-              Dipl. Oecotrophologin
-            </p>
-          </v-col>
-          <v-col
-            cols="12"
-            md="6"
-            xl="5"
-          >
-            <a
-              href="tel:01792 242543"
-              target="_blank"
-              class="body-1 font-weight-medium"
-            >+ 1792 242543</a>
-            <br>
-            <a
-              href="mailto:info@claudiaeck.de"
-              target="_blank"
-              class="body-1 font-weight-medium"
-            >info@claudiaeck.de</a>
-          </v-col>
-        </v-row>
-      </v-col>
-      <v-col
-        cols="11"
-        md="6"
-        lg="6"
-        xl="5"
-      >
-        <v-row
-          class="mt-4"
-          align="end"
+        <v-col
+          cols="11"
+          md="6"
+          lg="6"
+          xl="5"
         >
-          <v-col>
-            <p
-              class="body-1 font-weight-medium text-right"
+          <v-row class="mt-4">
+            <v-col
+              cols="12"
+              md="6"
+              xl="5"
             >
-              Persönlichkeitscoaching &
-            </p>
-            <p
-              class="body-1 font-weight-medium text-right"
+              <p class="body-1 font-weight-medium">
+                Persönlichkeitscoaching &
+              </p>
+              <p class="body-1 font-weight-medium">
+                Unternehmenscoaching Bonn
+              </p>
+            </v-col>
+            <v-col
+              cols="12"
+              sm="6"
+              xl="5"
             >
-              Unternehmenscoaching Bonn
-            </p>
-          </v-col>
-        </v-row>
-      </v-col>
-    </v-row>
-  </v-container>
+              <p class="body-1 font-weight-medium">
+                Claudia Eck
+              </p>
+              <p class="body-1 font-weight-medium">
+                Systemischer Coach
+              </p>
+              <p class="body-1 font-weight-medium">
+                Dipl. Oecotrophologin
+              </p>
+            </v-col>
+          </v-row>
+          <v-row align="end">
+            <v-col
+              cols="12"
+              md="6"
+              xl="5"
+            >
+              <p class="body-1 font-weight-medium">
+                Claudia Eck
+              </p>
+              <p class="body-1 font-weight-medium">
+                Systemischer Coach
+              </p>
+              <p class="body-1 font-weight-medium">
+                Dipl. Oecotrophologin
+              </p>
+            </v-col>
+            <v-col
+              cols="12"
+              md="6"
+              xl="5"
+            >
+              <a
+                href="tel:01792 242543"
+                target="_blank"
+                class="body-1 font-weight-medium"
+              >+ 1792 242543</a>
+              <br>
+              <a
+                href="mailto:info@claudiaeck.de"
+                target="_blank"
+                class="body-1 font-weight-medium"
+              >info@claudiaeck.de</a>
+            </v-col>
+          </v-row>
+        </v-col>
+        <v-col
+          cols="11"
+          md="6"
+          lg="6"
+          xl="5"
+        >
+          <v-row
+            class="mt-4 justify-end"
+            align="end"
+          >
+            <v-col class="footer__section-links">
+              <v-row
+                class="ml-auto pa-0"
+              >
+                <v-col>
+                  <v-img
+                    :src="require('~/assets/images/social-icons/facebook.svg')"
+                    alt="facebook"
+                    width="48px"
+                  />
+                </v-col>
+                <v-col>
+                  <v-img
+                    :src="require('~/assets/images/social-icons/twitter.svg')"
+                    alt="twitter"
+                    width="48px"
+                  />
+                </v-col>
+                <v-col>
+                  <v-img
+                    :src="require('~/assets/images/social-icons/xing.svg')"
+                    alt="xing"
+                    width="48px"
+                  />
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col cols="6" class="text-left">
+                  <router-link
+                    to="/"
+                    class="text-lowercase body-1 footer__button"
+                  >
+                    Impressum
+                  </router-link>
+                </v-col>
+                <v-col cols="6" class="text-right">
+                  <router-link
+                    to="/"
+                    class="text-lowercase body-1 footer__button"
+                  >
+                    Datenschutz
+                  </router-link>
+                </v-col>
+              </v-row>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
@@ -112,5 +147,14 @@ export default {
 <style scoped lang="scss">
 .footer {
   width: 100%;
+
+  &__section-links {
+    max-width: 230px;
+  }
+
+  &__button {
+    color: #2C2C2C;
+    text-decoration: none;
+  }
 }
 </style>
