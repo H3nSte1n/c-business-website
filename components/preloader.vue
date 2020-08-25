81b2bee2c332b6c2f8e6827ad9ed642c435c1ad9
Henry Steinhauer <henry.steinhauer@i22.de>
2020-08-25 16:50:46 +0200

    feat: Add preloader component

---
+++ components/preloader.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div v-if="loading" class="loader">
+    </div>
+    <slot v-else />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    loading: {
+      type: Boolean,
+      required: true,
+      default: true
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.loader {
+  border: 6px solid #f3f3f3;
+  border-top: 16px solid black;
+  border-radius: 50%;
+  width: 120px;
+  height: 120px;
+  animation: spin 2s linear infinite;
+  position: relative;
+  left: 50%;
+  top: 50%;
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+</style>
