580537a313dd79d9b95cc23aa31c0999d8ef08ee
Henry Steinhauer <henry.steinhauer@i22.de>
2020-09-08 22:47:10 +0200

    feat: create 404 page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,19 +1,35 @@
 <template>
-  <v-app dark>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/">
-      Home page
-    </NuxtLink>
+  <v-app>
+    <v-container class="error__container">
+      <v-row justify="center">
+        <v-col cols="12" sm="10" md=6 lg="4">
+          <v-img alt="404" :src="require('@/assets/images/404-schriftzug.svg')" />
+        </v-col>
+      </v-row>
+      <v-row justify="center">
+        <v-col cols="12" sm="10" md="8" lg="6">
+          <h1 class="error__headline text-center">Die von Ihnen aufgerufene Seite existiert nicht.</h1>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <router-link
+            to="/"
+            class="button body-1 font-weight-bold"
+          >
+            Zurück zur Startseite
+          </router-link>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-app>
 </template>
 
 <script>
+import ButtonEvents from '@/mixins/buttonEvents';
+
 export default {
+  mixins: [ButtonEvents],
   layout: 'empty',
   props: {
     error: {
@@ -37,8 +53,43 @@ export default {
 }
 </script>
 
-<style scoped>
-h1 {
-  font-size: 20px;
+<style scoped lang="scss">
+@import '@/scss/colors.scss';
+
+.error {
+  &__container {
+    height: 100vh;
+  }
+  &__headline {
+    font-size: 3rem;
+    font-weight: 300;
+  }
+}
+
+.button {
+  max-width: 200px;
+  color: $black;
+  text-decoration: none;
+  display: block;
+  margin: 0 auto;
+
+  &--disabled {
+    opacity: 0.2;
+  }
+
+  &:active, &:focus, &:visited, &:hover {
+    color: $black;
+    cursor: none;
+  }
+
+  &--mobile-active {
+    @media screen and (max-width: 1264px) {
+      text-decoration: none;
+      padding: 10px;
+      border: 2px solid $black;
+      border-radius: 25px;
+      color: $black;
+    }
+  }
 }
 </style>
