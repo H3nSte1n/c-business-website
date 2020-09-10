89c14f908c57d37503254028a32aaf1a7d631cd7
Henry Steinhauer <henry.steinhauer@i22.de>
2020-09-10 19:15:43 +0200

    feat: Replace components

--- pages/ueber-mich.vue
+++ pages/ueber-mich.vue
@@ -8,20 +8,20 @@
       v-for="(content, key) in teasers"
       :key="key"
     >
-      <Teaser
-        :color="content.color"
+      <Stripe
         :content="content.content"
+        :b-color="content.color"
       />
     </div>
   </v-container>
 </template>
 
 <script>
-import Teaser from '@/components/teaser';
+import Stripe from '@/components/stripe';
 import Header from '@/components/header';
 
 export default {
-  components: {Header, Teaser},
+  components: {Header, Stripe},
   transition: "swipe",
   data() {
     return {
@@ -38,29 +38,29 @@ export default {
           color: "yellow",
           content: {
             headline: 'Meine ganz private Seite',
-            desc: 'Mich interessiert einfach das Dazwischen. Meistens ist es nicht sichtbar, aber es schwingt in jeder Geste mit. Das kann ein versteckter Wunsch, eine stille Leidenschaft oder eine verborgene Stärke sein. Wenn diese heimlichen Schätze sichtbar werden, ist etwas passiert. Nein kein Wunder, aber eine wohlwollende Umgebung, die es möglich macht.\n\n Goethe hat das schon sehr früh erkannt:',
-            quote: 'Behandle die Menschen so, als ob sie das wären, was sie sein könnten. Und hilf ihnen das zu werden, was sie sein könnten.“'
+            desc_1: 'Mich interessiert einfach das Dazwischen. Meistens ist es nicht sichtbar, aber es schwingt in jeder Geste mit. Das kann ein versteckter Wunsch, eine stille Leidenschaft oder eine verborgene Stärke sein. Wenn diese heimlichen Schätze sichtbar werden, ist etwas passiert. Nein kein Wunder, aber eine wohlwollende Umgebung, die es möglich macht.\n\n Goethe hat das schon sehr früh erkannt:',
+            desc_2: 'Behandle die Menschen so, als ob sie das wären, was sie sein könnten. Und hilf ihnen das zu werden, was sie sein könnten.“'
           }
         },
         {
           color: "yellow",
           content: {
             headline: 'Mein Werden, mein Wirken, meine Werte',
-            desc: 'Eine meiner wohlwollenden Umgebungen war meine Schule. Man lernt nicht alles in der Schule, aber ich habe zum Glück das Wichtigste mitgenommen: Mut für ein selbstbestimmtes Leben und mir treu zu bleiben. Daher bin ich immer auf der Suche WER ich bin, WAS will ich und WAS ich kann. Schon als Kind bin ich fragend und staunend durch die Welt gegangen. Dabei habe ich sehr früh gemerkt, dass ich durch meine Fragen Menschen zum Nachdenken und Explorieren bringe.  Aber auch ich bin zum Reflektieren gekommen. Durch meine persönliche Entdeckungsreise habe ich zu meinen Werten und meiner Haltung gefunden: Wertschätzung, Respekt, Mut, Sinnhaftigkeit, Genuss, Ausdauer, Spaß. Das Bewusstwerden meiner Werte und das Wissen wie mein Verhalten und Handeln wirkt, geben mir eine tragende Qualität meiner Arbeit als Systemischer Coach.',
+            desc_1: 'Eine meiner wohlwollenden Umgebungen war meine Schule. Man lernt nicht alles in der Schule, aber ich habe zum Glück das Wichtigste mitgenommen: Mut für ein selbstbestimmtes Leben und mir treu zu bleiben. Daher bin ich immer auf der Suche WER ich bin, WAS will ich und WAS ich kann. Schon als Kind bin ich fragend und staunend durch die Welt gegangen. Dabei habe ich sehr früh gemerkt, dass ich durch meine Fragen Menschen zum Nachdenken und Explorieren bringe.  Aber auch ich bin zum Reflektieren gekommen. Durch meine persönliche Entdeckungsreise habe ich zu meinen Werten und meiner Haltung gefunden: Wertschätzung, Respekt, Mut, Sinnhaftigkeit, Genuss, Ausdauer, Spaß. Das Bewusstwerden meiner Werte und das Wissen wie mein Verhalten und Handeln wirkt, geben mir eine tragende Qualität meiner Arbeit als Systemischer Coach.',
           }
         },
         {
-          color: "light-blue",
+          color: "blue",
           content: {
             headline: 'Meine Lernkurve im Beruf',
-            desc: 'Geradlinig war mein Weg nicht: Mit einem Germanistikstudium angefangen und mit Oecotrophologie abgeschlossen. Die ersten Berufsjahre habe ich als Produktmanager und Führungskraft in der Lebensmittelindustrie gearbeitet und später in einem Fachverlag für Ernährung, Gesundheit und Bildung als Redakteurin. Ich bin durch viele Abteilungen und viele Umstrukturierungsprozesse gegangen und bin durch eine Selbsterkenntnis reifer: Veränderungsprozesse haben Magie und ein kreativer Geist von Facilitation weht durch die Abteilungen. Das ist eine lebendige und dynamische Zeit mit offenem Ausgang und wenn es gut begleitet ist, ist alles im Fluss und in den Händen der Mitarbeiter. Ich habe miterlebt, dass Unternehmen das aushalten können, wenn sie dem Prozess und ihren Mitarbeitern vertrauen. Das für jeden erfahrbar zu machen, daran hängt mein Herzblut in meinem Beruf als Systemischer Coach und Veränderungsmanager – meine Selbstbestimmung.'
+            desc_1: 'Geradlinig war mein Weg nicht: Mit einem Germanistikstudium angefangen und mit Oecotrophologie abgeschlossen. Die ersten Berufsjahre habe ich als Produktmanager und Führungskraft in der Lebensmittelindustrie gearbeitet und später in einem Fachverlag für Ernährung, Gesundheit und Bildung als Redakteurin. Ich bin durch viele Abteilungen und viele Umstrukturierungsprozesse gegangen und bin durch eine Selbsterkenntnis reifer: Veränderungsprozesse haben Magie und ein kreativer Geist von Facilitation weht durch die Abteilungen. Das ist eine lebendige und dynamische Zeit mit offenem Ausgang und wenn es gut begleitet ist, ist alles im Fluss und in den Händen der Mitarbeiter. Ich habe miterlebt, dass Unternehmen das aushalten können, wenn sie dem Prozess und ihren Mitarbeitern vertrauen. Das für jeden erfahrbar zu machen, daran hängt mein Herzblut in meinem Beruf als Systemischer Coach und Veränderungsmanager – meine Selbstbestimmung.'
           }
         },
         {
-          color: "light-blue",
+          color: "blue",
           content: {
             headline: 'Mein Spaß und meine Hobbies',
-            desc: 'Ich liebe es als Coach zu sehen, wie sich Menschen auf ihrem selbstbestimmten Weg entfalten und vital werden, ihre Ressourcen und Kompetenz aktivieren. Als Oecotrophologin mag ich den etwas aus der Mode gekommenen Begriff der gesundheitlichen Selbstfürsorge. Er sagt so schön, dass jeder für sich selbst sorgen darf. Ich sorge für mich, indem ich genussvoll esse, über die Alpen radle, französisch lerne, alleine oder mit Freunden in einem Café sitze und als „Mamapreneur“ Spaß habe.'
+            desc_1: 'Ich liebe es als Coach zu sehen, wie sich Menschen auf ihrem selbstbestimmten Weg entfalten und vital werden, ihre Ressourcen und Kompetenz aktivieren. Als Oecotrophologin mag ich den etwas aus der Mode gekommenen Begriff der gesundheitlichen Selbstfürsorge. Er sagt so schön, dass jeder für sich selbst sorgen darf. Ich sorge für mich, indem ich genussvoll esse, über die Alpen radle, französisch lerne, alleine oder mit Freunden in einem Café sitze und als „Mamapreneur“ Spaß habe.'
           }
         },
       ]
