fcc0c40d729f5804ea2eb69416154131f7dfe1a9
Henry Steinhauer <henry.steinhauer@i22.de>
2020-09-10 14:25:46 +0200

    feat: Edit 404 styling

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,21 +1,13 @@
 <template>
-  <v-app>
+  <v-app ref="app">
     <v-container class="error__container">
       <v-row justify="center">
-        <v-col cols="12" sm="10" md=6 lg="4">
-          <v-img alt="404" :src="require('@/assets/images/404-schriftzug.svg')" />
-        </v-col>
-      </v-row>
-      <v-row justify="center">
-        <v-col cols="12" sm="10" md="8" lg="6">
-          <h1 class="error__headline text-center">Die von Ihnen aufgerufene Seite existiert nicht.</h1>
-        </v-col>
-      </v-row>
-      <v-row>
         <v-col>
+          <h1 class="error__headline">404</h1>
+          <p class="error__subline text-center">Die von Ihnen aufgerufene<br> Seite existiert nicht.</p>
           <router-link
             to="/"
-            class="button body-1 font-weight-bold"
+            class="button body-1 font-weight-bold text-center"
           >
             Zurück zur Startseite
           </router-link>
@@ -58,9 +50,17 @@ export default {
 
 .error {
   &__container {
-    height: 100vh;
+    top: 50%;
+    position: relative;
+    transform: translateY(-60%);
   }
   &__headline {
+    text-align: center;
+    font-weight: 900;
+    font-size: 9rem;
+    opacity: 0.1;
+  }
+  &__subline {
     font-size: 3rem;
     font-weight: 300;
   }
@@ -71,7 +71,12 @@ export default {
   color: $black;
   text-decoration: none;
   display: block;
-  margin: 0 auto;
+  margin: 40px auto;
+  text-decoration: none;
+  padding: 10px;
+  border: 2px solid $black;
+  border-radius: 25px;
+  color: $black;
 
   &--disabled {
     opacity: 0.2;
@@ -80,16 +85,6 @@ export default {
   &:active, &:focus, &:visited, &:hover {
     color: $black;
     cursor: none;
-  }
-
-  &--mobile-active {
-    @media screen and (max-width: 1264px) {
-      text-decoration: none;
-      padding: 10px;
-      border: 2px solid $black;
-      border-radius: 25px;
-      color: $black;
-    }
   }
 }
 </style>
