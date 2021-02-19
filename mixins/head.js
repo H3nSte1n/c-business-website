9aa4d5cf64e1eeb135fddbefa8f81d944dbf0a6a
Henry Steinhauer <henry.steinhauer@i22.de>
2021-02-19 10:20:51 +0100

    test

--- mixins/head.js
+++ mixins/head.js
@@ -5,19 +5,17 @@ export default {
       social_mediaDescription: 'Als Systemischer Coach und Diplom-Oecotrophologin verfolge ich einen ganzheitlichen Coaching- und Präventionsansatz. Ich bringe Themen wie Gesundheit und persönliche Entwicklung für einen gesundheitsfördernden Lebens- und Arbeitsstil zusammen.',
     }
   },
-  head() {
-    return {
-      title: this.getTitle(),
-      /*meta: [
-        { name: 'description', content: this.getDescription() },
-        { name: 'robots', content: this.robots ? 'index, follow' : 'noindex, nofollow' },
-        { property: 'og:type', content: 'website' },
-        { property: 'og:site_name', content: 'Claudia Eck' },
-        { property: 'og:title', content: this.getTitle() },
-        { property: 'og:description', content: this.getSocialMediaDescription() },
-        { property: 'og:image', content: '' },
-      ]*/
-    }
+  head: {
+    title: this.getTitle(),
+    meta: [
+      { name: 'description', content: this.getDescription() },
+      { name: 'robots', content: this.robots ? 'index, follow' : 'noindex, nofollow' },
+      { property: 'og:type', content: 'website' },
+      { property: 'og:site_name', content: 'Claudia Eck' },
+      { property: 'og:title', content: this.getTitle() },
+      { property: 'og:description', content: this.getSocialMediaDescription() },
+      { property: 'og:image', content: '' },
+    ]
   },
   methods: {
     getTitle() {
