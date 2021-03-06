25ec97330ca6adb4ccfbfcc4cfdb9b9d18be92ac
Henry Steinhauer <henry.steinhauer@i22.de>
2021-03-06 14:23:45 +0100

    hotfix

--- components/view/teaser.vue
+++ components/view/teaser.vue
@@ -1,26 +1,26 @@
 <template>
   <v-container>
-    <v-lazy
-      v-model="isActive"
-      :options="{
-        threshold: .5
-      }"
-      min-height="200"
-      transition="fade-transition"
+    <v-row
+      justify="space-around"
+      align="center"
+      flex
+      :class="$vuetify.breakpoint.mdAndUp ? 'py-12': 'py-1'"
     >
-      <v-row
-        justify="space-around"
-        align="center"
-        flex
-        :class="$vuetify.breakpoint.mdAndUp ? 'py-12': 'py-1'"
+      <v-col
+        cols="12"
+        sm="12"
+        md="5"
+        xl="4"
+        :order-md="content.order ? content.order : 'first'"
+        class="my-9 teaser__img-container"
       >
-        <v-col
-          cols="12"
-          sm="12"
-          md="5"
-          xl="4"
-          :order-md="content.order ? content.order : 'first'"
-          class="my-9 teaser__img-container"
+        <v-lazy
+          v-model="isActive"
+          :options="{
+            threshold: .5
+          }"
+          min-height="200"
+          transition="fade-transition"
         >
           <v-img
             :src="content.img.src"
@@ -29,36 +29,36 @@
             :max-width="$vuetify.breakpoint.smAndDown ? '200px': 'none'"
             class="mx-auto my-12"
           />
-        </v-col>
-        <v-col
-          cols="12"
-          sm="12"
-          md="5"
-          xl="4"
+        </v-lazy>
+      </v-col>
+      <v-col
+        cols="12"
+        sm="12"
+        md="5"
+        xl="4"
+      >
+        <div
+          class="teaser__invisible"
+          :class="{'teaser__invisible-enable': isClicked}"
+        />
+        <h2 class="display-1 font-weight-light teaser__headline">
+          {{ content.headline }}
+        </h2>
+        <p class="body-1 font-weight-bold mt-2 mb-10">
+          {{ content.subline }}
+        </p>
+        <p
+          class="body-1 mb-6"
+          v-html="content.text"
+        />
+        <router-link
+          :to="content.button.link"
+          class="text-lowercase button button--mobile-active body-1 font-weight-bold"
         >
-          <div
-            class="teaser__invisible"
-            :class="{'teaser__invisible-enable': isClicked}"
-          />
-          <h2 class="display-1 font-weight-light teaser__headline">
-            {{ content.headline }}
-          </h2>
-          <p class="body-1 font-weight-bold mt-2 mb-10">
-            {{ content.subline }}
-          </p>
-          <p
-            class="body-1 mb-6"
-            v-html="content.text"
-          />
-          <router-link
-            :to="content.button.link"
-            class="text-lowercase button button--mobile-active body-1 font-weight-bold"
-          >
-            {{ content.button.text }}
-          </router-link>
-        </v-col>
-      </v-row>
-    </v-lazy>
+          {{ content.button.text }}
+        </router-link>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
