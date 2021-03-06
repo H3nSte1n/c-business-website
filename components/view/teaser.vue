bc36cda44e4084bb2d18760ac8695ab81816f6f3
Henry Steinhauer <henry.steinhauer@i22.de>
2021-03-06 13:39:29 +0100

    feat: Try vuetify lazy loading

--- components/view/teaser.vue
+++ components/view/teaser.vue
@@ -6,49 +6,57 @@
       flex
       :class="$vuetify.breakpoint.mdAndUp ? 'py-12': 'py-1'"
     >
-      <v-col
-        cols="12"
-        sm="12"
-        md="5"
-        xl="4"
-        :order-md="content.order ? content.order : 'first'"
-        class="my-9 teaser__img-container"
+      <v-lazy
+        :options="{
+          threshold: .5
+        }"
+        min-height="200"
+        transition="fade-transition"
       >
-        <v-img
-          :src="content.img.src"
-          :alt="content.img.alt"
-          :width="$vuetify.breakpoint.smAndDown ? '40%': '100%'"
-          :max-width="$vuetify.breakpoint.smAndDown ? '200px': 'none'"
-          class="mx-auto my-12"
-        />
-      </v-col>
-      <v-col
-        cols="12"
-        sm="12"
-        md="5"
-        xl="4"
-      >
-        <div
-          class="teaser__invisible"
-          :class="{'teaser__invisible-enable': isClicked}"
-        />
-        <h2 class="display-1 font-weight-light teaser__headline">
-          {{ content.headline }}
-        </h2>
-        <p class="body-1 font-weight-bold mt-2 mb-10">
-          {{ content.subline }}
-        </p>
-        <p
-          class="body-1 mb-6"
-          v-html="content.text"
-        />
-        <router-link
-          :to="content.button.link"
-          class="text-lowercase button button--mobile-active body-1 font-weight-bold"
+        <v-col
+          cols="12"
+          sm="12"
+          md="5"
+          xl="4"
+          :order-md="content.order ? content.order : 'first'"
+          class="my-9 teaser__img-container"
         >
-          {{ content.button.text }}
-        </router-link>
-      </v-col>
+          <v-img
+            :src="content.img.src"
+            :alt="content.img.alt"
+            :width="$vuetify.breakpoint.smAndDown ? '40%': '100%'"
+            :max-width="$vuetify.breakpoint.smAndDown ? '200px': 'none'"
+            class="mx-auto my-12"
+          />
+        </v-col>
+        <v-col
+          cols="12"
+          sm="12"
+          md="5"
+          xl="4"
+        >
+          <div
+            class="teaser__invisible"
+            :class="{'teaser__invisible-enable': isClicked}"
+          />
+          <h2 class="display-1 font-weight-light teaser__headline">
+            {{ content.headline }}
+          </h2>
+          <p class="body-1 font-weight-bold mt-2 mb-10">
+            {{ content.subline }}
+          </p>
+          <p
+            class="body-1 mb-6"
+            v-html="content.text"
+          />
+          <router-link
+            :to="content.button.link"
+            class="text-lowercase button button--mobile-active body-1 font-weight-bold"
+          >
+            {{ content.button.text }}
+          </router-link>
+        </v-col>
+      </v-lazy>
     </v-row>
   </v-container>
 </template>
